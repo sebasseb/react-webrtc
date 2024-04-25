3545d7fdfbe57c8680ad4e681ef94224f55d7036
sebasseb <sebastian.escobar.b@mail.pucv.cl>
2024-04-25 05:54:32 -0400

    ip publica server signaling

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("54.198.193.242:8000"), []);
 
   return (
     <SocketContext.Provider value={socket}>
