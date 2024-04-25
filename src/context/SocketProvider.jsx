05b2b48efc1b119d38dd91161a97e1f68bf25885
sebasseb <sebastian.escobar.b@mail.pucv.cl>
2024-04-25 06:19:10 -0400

    change socket ip

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://fir-puddle-catsup.glitch.me:8000"), []);
+  const socket = useMemo(() => io("https://fir-puddle-catsup.glitch.me"), []);
 
   return (
     <SocketContext.Provider value={socket}>
